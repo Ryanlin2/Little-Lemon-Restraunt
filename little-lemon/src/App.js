99732c47d09ba3765984ecb25c35f14221f7d587
Ryan_Lin2 <Ryanlin0861@gmail.com>
2025-06-20 16:32:12 -0400

    start header

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <nav>nav</nav>
+      <main>main</main>
+      <footer>footer</footer>
+    </>
   );
 }
 
