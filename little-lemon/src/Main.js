d359a21152ba25ee3b5ad50b2380f163019f8ef6
Ryan_Lin2 <Ryanlin0861@gmail.com>
2025-06-25 22:39:16 -0400

    floating image

--- little-lemon/src/Main.js
+++ little-lemon/src/Main.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import logo from './assets/Logo.svg'
+import restauranfood from './assets/restauranfood.jpg'
 
 function Main(){
     return (
         <div className="container">
                 <div className="nav-list">
-                                <div className="section">
-                    <h1>Little Lemon</h1>
-                </div>
+                    <div className="section">
+                        <h1>Little Lemon</h1>
+                        <h3>Chicago</h3>
+                        <p>
+                            We are family owned Mediterranean restraunt, focused on treaditional recipes served with a modern twist.
+                        </p>
+                    </div>
                 <div className="section">
-                    <h1>Little Lemon</h1>
+                    <div className="section image-wrapper">
+                    <img src={restauranfood} alt="server" className="serverImg" />
+                    </div>
                 </div>
             </div>
         </div>
